0bee2c18f0c62543fede9dd96ec2b1aad6b252f0
Sergey <scandy_boss@ukr.net>
2019-03-30 20:19:25 +0200

    Updates

---
+++ precache-manifest.e718095862a67ccb431778d1fb11d219.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "951d3ec74978e9f2f266bc12b77f3516",
+    "url": "/forecast-app/static/media/polygon.951d3ec7.jpg"
+  },
+  {
+    "revision": "9f1ef7bf1016f75600373d085b2ebb8c",
+    "url": "/forecast-app/static/media/Vector_green.9f1ef7bf.svg"
+  },
+  {
+    "revision": "91c050a90addb996e46aa397c520d19b",
+    "url": "/forecast-app/static/media/Vector.91c050a9.svg"
+  },
+  {
+    "revision": "1b8ff24b22f74ad27fb6",
+    "url": "/forecast-app/static/js/runtime~main.cfa6f48a.js"
+  },
+  {
+    "revision": "8987ce641d95151af08d",
+    "url": "/forecast-app/static/js/main.0fdb6207.chunk.js"
+  },
+  {
+    "revision": "bfe32cb0679a980c230c",
+    "url": "/forecast-app/static/js/2.36aae0d1.chunk.js"
+  },
+  {
+    "revision": "8987ce641d95151af08d",
+    "url": "/forecast-app/static/css/main.6e406e7d.chunk.css"
+  },
+  {
+    "revision": "5fcaf2ec2a095a4cf9d1675f9b2f23af",
+    "url": "/forecast-app/index.html"
+  }
+];
